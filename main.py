7aa13937ee8ae789e89d66e24b4a6b2c9c1565b4
Gabe Aquino <lq90575@umbc.edu>
2024-01-10 02:55:41 -0500

    All data hase been added up to January 9, 2024

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 from functions import *
 
 execute_result()
-
-
-
-# result = read_command()
-
-# for app in result['app_data']:
-#     print(f"""
-#                     INSERT INTO App_Data (date_str, app_name, time_duration)
-#                     VALUES ('{result['date_str']}', '{app}', {result['app_data'][app]})
-#     """) # RESUME
